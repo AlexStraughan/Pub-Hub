5561881a1a63d846c14105dbb2d9c431e8e06887
Cal Norman <calnorman1@gmail.com>
2022-06-07 17:38:23 +0100

    finished first stage of geocoding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_06_135515) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_161341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,13 +25,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_135515) do
     t.string "name"
     t.string "address"
     t.string "description"
-    t.string "brand"
-    t.boolean "wheelchair"
-    t.string "smoking"
-    t.float "latitude"
-    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "stops", force: :cascade do |t|
