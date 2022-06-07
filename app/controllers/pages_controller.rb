b9fb34a1d74873ee4634112472c4172450833105
Cal Norman <calnorman1@gmail.com>
2022-06-07 11:33:05 +0100

    finished v-1 pages controller and pubs controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: :home
 
   def home
+    @pubs = Pub.limit(20).order(:desc)
   end
 end
--- app/controllers/pubs_controller.rb
+++ app/controllers/pubs_controller.rb
@@ -1,2 +1,22 @@
 class PubsController < ApplicationController
+  before_action :find_by_id, only: [:show]
+  skip_before_action :authenticate_user!, only: %i[index show]
+
+  def index
+    @pubs = Pubs.all
+  end
+
+  def show
+    @pub = Pub.find(params[:id])
+  end
+
+  private
+
+  def find_by_id
+    @pub = Pub.find(params[:id])
+  end
+
+  def pub_params
+    params.require(:pub).permit(:name, :address, :description)
+  end
 end
